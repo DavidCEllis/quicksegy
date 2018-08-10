89b21d05fc7a84fe1542a3f0b36109d47672755b
davidcellis <ducksual@gmail.com>
2018-08-10 10:42:35 +0100

    Tests for structutils

--- tests/test_structutils.py
+++ tests/test_structutils.py
@@ -1,82 +1,60 @@
-import math
 import struct
 
-from hypothesis import given
-from hypothesis.strategies import booleans, data, floats, integers, lists, sampled_from
+import pytest
 
-from quicksegy.struct_utils import StructPair, SingleStruct, MultiStruct
-
-
-hypothesis_types = {
-    'b': integers(min_value=-2**7, max_value=2**7-1),
-    'B': integers(min_value=0, max_value=2**8-1),
-    'h': integers(min_value=-2**15, max_value=2**15-1),
-    'H': integers(min_value=0, max_value=2**16-1),
-    'l': integers(min_value=-2**31, max_value=2**31-1),
-    'L': integers(min_value=0, max_value=2**32-1),
-    'q': integers(min_value=-2**63, max_value=2**63-1),
-    'Q': integers(min_value=0, max_value=2**64-1),
-    'f': floats(min_value=-10**38, max_value=10**38,
-                allow_infinity=False, allow_nan=False),
-    'd': floats(min_value=-10**300, max_value=10**300,
-                allow_infinity=False, allow_nan=False),
-}
+from quicksegy.struct_utils import StructPair, MultiStruct
 
 
-@given(integers(min_value=0), sampled_from('bBhHlLqQfd'), booleans())
-def test_structpair(offset, ctype, ibm_float):
-    sp = StructPair(offset, ctype, ibm_float)
-    assert sp.offset == offset
-    assert sp.ctype == ctype
-    assert sp.ibm_float == ibm_float
+@pytest.fixture
+def demo_data():
+    testvalsp1 = [2, -4, 0.5, 2**40]
+    testvalsp2 = [-2**20, 16, 10.25]
 
-    rsp = eval(repr(sp))
+    test_struct = struct.pack('>BhfQ', *testvalsp1)
+    test_struct += struct.pack('>bbbb', 0, 0, 0, 0)
+    test_struct += struct.pack('>lHd', *testvalsp2)
 
-    assert isinstance(rsp, StructPair)
-    assert sp.offset == rsp.offset
-    assert sp.ctype == rsp.ctype
-    assert sp.ibm_float == rsp.ibm_float
+    return test_struct
 
 
-@given(data())
-def test_singlestruct(data):
-    # Generate a random fmt string
-    structfmtlist = data.draw(lists(sampled_from('bBhHlLqQfd'), min_size=1, max_size=40))
-    endian = '>' if data.draw(booleans()) else '<'
-    structfmt = endian + ''.join(structfmtlist)
+@pytest.fixture
+def demo_multi():
+    struct_dict = {
+        'v1': StructPair(0, 'B'),
+        'v2': StructPair(1, 'h'),
+        'v3': StructPair(3, 'f'),
+        'v4': StructPair(7, 'Q'),
+        'v5': StructPair(19, 'l'),
+        'v6': StructPair(23, 'H'),
+        'v7': StructPair(25, 'd'),
+    }
 
-    keys = [str(i) for i, _ in enumerate(structfmtlist)]
-    values = [data.draw(hypothesis_types[key]) for key in structfmtlist]
-
-    expected = {key: value for key, value in zip(keys, values)}
-
-    teststruct = struct.Struct(structfmt)
-    testpacked = teststruct.pack(*values)
-
-    unpacker = SingleStruct(teststruct, keys, offset=0)
-
-    actual = unpacker.unpack(testpacked)
-
-    for key in actual.keys():
-        if isinstance(actual[key], float):
-            assert math.isclose(expected[key], actual[key], rel_tol=0.001)
-        else:
-            assert expected[key] == actual[key]
+    ms = MultiStruct(struct_dict)
+    return ms
 
 
-@given(data())
-def test_singlestruct_repr(data):
-    structfmtlist = data.draw(lists(sampled_from('bBhHlLqQfd'), min_size=1, max_size=40))
-    endian = '>' if data.draw(booleans()) else '<'
-    structfmt = endian + ''.join(structfmtlist)
-    teststruct = struct.Struct(structfmt)
-    keys = [str(i) for i, _ in enumerate(structfmtlist)]
-    offset = data.draw(integers(min_value=0, max_value=1000))
+def test_multistruct_build(demo_multi):
 
-    expected = SingleStruct(teststruct, keys, offset=offset)
+    ms = demo_multi
 
-    result = eval(repr(expected))
+    assert len(ms.structs) == 2
+    assert ms.structs[0].offset == 0
+    assert ms.structs[1].offset == 19
+    assert ms.structs[0].struct_.format == '>BhfQ'
+    assert ms.structs[1].struct_.format == '>lHd'
+    assert ms.structs[0].names == ['v1', 'v2', 'v3', 'v4']
+    assert ms.structs[1].names == ['v5', 'v6', 'v7']
 
-    assert expected.struct_.format == result.struct_.format
-    assert expected.names == result.names
-    assert expected.offset == result.offset
+
+def test_multistruct_unpack(demo_multi, demo_data):
+    expected = {
+        'v1': 2,
+        'v2': -4,
+        'v3': 0.5,
+        'v4': 2**40,
+        'v5': -2**20,
+        'v6': 16,
+        'v7': 10.25,
+    }
+
+    assert demo_multi.unpack(demo_data) == expected
